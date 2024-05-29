5710f72d0f6d6b8302b63be642d9039fc9ce5103
st-minju <kimminju0115@gmail.com>
2024-05-29 17:49:04 +0900

    java start

---
+++ java study/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+	/**
+	 * @param
+	 * @author
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("good");
+		System.out.print(1);
+	}
+}
