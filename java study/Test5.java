514cd166396f0a06ef8108318174d50d1d012eeb
st-minju <kimminju0115@gmail.com>
2024-05-31 17:38:44 +0900

    study

--- java study/Test5.java
+++ java study/Test5.java
@@ -2,14 +2,16 @@ public class Test5 {
 
   /** 2024-05-31 String */
   public static void main(String[] args) {
-    /*String firstName = "minju";
+    String firstName = "minju";
     String secondName = "kim";
     String phone = "Iphone";
     String status = "hungry";
-    System.out.println("나는 " + secondName + " " + firstName + " 입니다.");\
-    */
+    System.out.println("나는 " + secondName + " " + firstName + " 입니다.");
 
-    /*String a = "EstSoft";
+    String a = "EstSoft";
+    System.out.println(a.equals("estsoft")); // false
+    System.out.println("소문자: " + a.indexOf("s")); // 4
+    System.out.println("대문자: " + a.indexOf("S")); // 4
     System.out.println(a.contains("Soft"));
 
     String b = "Hello world";
@@ -18,7 +20,7 @@ public class Test5 {
     System.out.println(b.charAt(7));
     System.out.println(b.charAt(8));
     System.out.println(b.replaceAll("world", "Spring"));
-    */
+    System.out.println(b.replace("world", "Spring"));
     String c = "WEEEE ASKE ;ND";
     System.out.println(c.substring(0, 3) + c.substring(8, 10) + c.substring(12, 14));
 
@@ -36,5 +38,17 @@ public class Test5 {
     int age2 = 45;
     String.format("%s의 나이는 %s세 입니다.", name1, age1);
     String.format("%s의 나이는 %s세 입니다.", name2, age2);
+
+    String str = "Hello";
+    String str2 = new String("Hello"); // 새로운 주소로 생성
+    String str3 = "Hello"; // String Constant Pool에 의해 같은 주소에 저장
+    String str4 = "Hello!";
+    System.out.println(str.equals(str2)); // true
+    System.out.println(str == str2); // false
+    System.out.println(str == str3); // true
+    System.out.println((str + "!") == str4); // false 애초에 str4가 다른 주소에 위치
+
+    System.out.println(e.concat("!") == (str + "!")); // false
+    System.out.println(e.concat("!") == str4); // false
   }
 }
--- java study/Test6.java
+++ java study/Test6.java
@@ -7,6 +7,8 @@ public class Test6 {
     result += "Java ";
     result += "World";
     System.out.println(result);
+    System.out.println(result.replace("hello", "~~~~"));
+    System.out.println(result.replaceAll("hello", "~~~~"));
 
     // StringBuilder or StringBuffer는 String의 메모리 낭비를 보완하기 위해 사용
     // 문자열을 수정 및 조작할 때 사용 권장
@@ -15,14 +17,14 @@ public class Test6 {
     sb.append("Hello ");
     sb.append("Java ");
     sb.append("World");
-    System.out.println(sb);
+    System.out.println(sb); // Hello Java World
     sb.insert(0, "Hi ");
-    System.out.println(sb);
+    System.out.println(sb); // Hi Hello Java World
 
     StringBuffer sf = new StringBuffer();
     sf.append("Hello ");
     sf.append("Java ");
     sf.append("World");
-    System.out.println(sf);
+    System.out.println(sf); // Hello Java World
   }
 }
