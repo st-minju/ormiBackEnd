b8dc19eb3e892481a74c1399cf20e2ec194b3a5c
st-minju <kimminju0115@gmail.com>
2024-06-04 15:54:51 +0900

    study

--- java study/ForTest.java
+++ java study/ForTest.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-
 public class ForTest {
   /** 2024-06-04 for문 */
   public static void main(String[] args) {
 
-    int sum = 0;
+    /*int sum = 0;
     for (int n = 1; n <= 100; n++) {
       if (n % 2 == 0) sum += n;
     }
@@ -17,7 +15,7 @@ public class ForTest {
     }
     System.out.println("합은: " + sum);
 
-    /*int fib1 = 1;
+    int fib1 = 1;
     int fib2 = 1;
     System.out.println("피보나치 수열 출력 Go!");
     for (int i = 0; i < 10; i += 2) {
@@ -25,7 +23,7 @@ public class ForTest {
       System.out.println(fib2);
       fib1 += fib2;
       fib2 += fib1;
-    }*/
+    }
     int[] fib = new int[10];
     fib[0] = 1;
     fib[1] = 1;
@@ -42,16 +40,37 @@ public class ForTest {
     }
 
     int[] arr = new int[100];
-    /*arr[0] = 1;
+    arr[0] = 1;
     arr[1] = 1;
     for (int i = 2; i < arr.length; i++) {
       arr[i] = arr[i - 1] + arr[i - 2];
-    }*/
+    }
 
     Arrays.setAll(arr, i -> (i < 2) ? 1 : arr[i - 1] + arr[i - 2]);
     System.out.println("피보나치 수열 출력 Go!");
     for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i]);
+    }*/
+
+    /*for (int i = 1; i <= 100; i++) {
+      if (i == 50) break;
+      System.out.println("***** i는 " + i);
+      for (int j = 2; j <= 100; j++) {
+        if (j == 80) break;
+        System.out.println("j는 " + j);
+      }
+    }*/
+
+    /*int[] scores = {1, 30, 20, 80, 15, 20, 30, 22};
+      int sum = 0;
+      for (int i : scores) {
+        sum += i;
+      }
+      System.out.println("총합: " + sum + " 평균: " + sum / scores.length);
+    */
+    String[] arr = {"okay2", "asbds", "good2", "ormiiiii", "abcde"};
+    for (String item : arr) {
+      if (item.length() >= 5) System.out.println(item);
     }
   }
 }
---
+++ java study/Gugudan.java
@@ -0,0 +1,15 @@
+public class Gugudan {
+  public static void main(String[] args) {
+    /*for (int i = 2; i <= 10; i++) {
+      for (int j = 1; j <= 10; j++) {
+        System.out.println(i + "X" + j + "=" + (i * j));
+      }
+    }*/
+
+    for (int i = 19; i >= 1; i--) {
+      for (int j = 19; j >= 1; j--) {
+        System.out.println(i + "X" + j + "=" + (i * j));
+      }
+    }
+  }
+}
