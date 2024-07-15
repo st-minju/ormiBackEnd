9923278386f0a2561974c1e78c7a2116ce633274
st-minju <kimminju0115@gmail.com>
2024-07-15 15:16:49 +0900

    Spring Study

---
+++ src/test/java/com/barabam/dailyquiz/DailyQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barabam.dailyquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
