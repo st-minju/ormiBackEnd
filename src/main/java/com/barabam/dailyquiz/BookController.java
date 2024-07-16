c45734bf5e677efe125ed716cbff31b7491cb9d4
st-minju <kimminju0115@gmail.com>
2024-07-17 02:57:42 +0900

    0716

--- src/main/java/com/barabam/dailyquiz/BookController.java
+++ src/main/java/com/barabam/dailyquiz/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,13 +18,13 @@ public class BookController {
 	private long bookId = 1L;
 
 	@GetMapping
-	public String Book(Model model){
+	public String listBooks(Model model){
 		model.addAttribute("books", books);
 		return "books";
 	}
 
 	@GetMapping("/new")
-	public String newBook(Model model) {
+	public String showAddBookForm(Model model) {
 		model.addAttribute("book", new Book());
 		return "addBook";
 	}
@@ -33,5 +34,41 @@ public class BookController {
 		book.setId(bookId++);
 		books.add(book);
 		return "redirect:/books";
+	}
+
+	@GetMapping("/editBook/{id}")
+	public String showEditForm(@PathVariable("id") long id, Model model) {
+		Book book = findBookById(id);
+		model.addAttribute("book", book);
+		return "editBook";
+	}
+
+	@PostMapping("/editBook/{id}")
+	public String editBook(@PathVariable("id") long id, @ModelAttribute Book updateBook) {
+		Book book = findBookById(id);
+    	updateBook(book, updateBook);
+		return "redirect:/books";
+	}
+
+	@PostMapping("/deleteBook/{id}")
+	public String deleteBook(@PathVariable("id") long id) {
+		Book book = findBookById(id);
+		books.remove(book);
+		return "redirect:/books";
+	}
+
+	private Book findBookById(long id) {
+		Book book = books.stream()
+			.filter(b -> b.getId() == id)
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("해당하는 아이디의 책이 없습니다."));
+		return book;
+	}
+
+	private void updateBook(Book book, Book updateBook) {
+		book.setTitle(updateBook.getTitle());
+		book.setAuthor(updateBook.getAuthor());
+		book.setPublicationYear(updateBook.getPublicationYear());
+
 	}
 }
