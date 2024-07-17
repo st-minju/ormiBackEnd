ac0fe03921cc9463645df444b36defc659f07bdc
st-minju <kimminju0115@gmail.com>
2024-07-17 11:29:00 +0900

    Spring Study

--- est-backend-5-master/src/main/java/net/chimaek/basic/BasicApplication.java
+++ est-backend-5-master/src/main/java/net/chimaek/basic/BasicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "net.chimaek.day0716")
+@SpringBootApplication(scanBasePackages = "net.chimaek.day0717")
 public class BasicApplication {
 
     @Value("${server.port}")
